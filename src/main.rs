261edd3566c3394f03af30a3fd4b2de08b18b4c3
coder-pig <aimeedeer@gmail.com>
2020-01-26 16:13:50 +0700

    RIB_AGENT

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use reqwest::blocking::Client;
 use reqwest::Method;
 use reqwest::header::USER_AGENT;
 
+static RIB_AGENT:&'static str = "";
 
 fn main() -> Result<(), reqwest::Error>{
 
     let client = Client::new();
     let builder = client.request(Method::GET, "https://api.github.com/repos/nervosnetwork/ckb/pulls");
-    let builder = builder.header(USER_AGENT, "foo");
+    let builder = builder.header(USER_AGENT, RIB_AGENT);
     let body = builder.send()?.text()?;
 
 
@@ -21,8 +22,6 @@ fn main() -> Result<(), reqwest::Error>{
     
     //println!("test");
     return Ok(());
-
-
 
 }
 
