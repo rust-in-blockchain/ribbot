b972d6e6d5649ffbd48d65facda7689cae35741d
coder-pig <aimeedeer@gmail.com>
2020-01-26 15:54:26 +0700

    add main

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+use reqwest::blocking::Client;
+use reqwest::Method;
+use reqwest::header::USER_AGENT;
+
+
+fn main() -> Result<(), reqwest::Error>{
+
+    let client = Client::new();
+    let builder = client.request(Method::GET, "https://api.github.com/repos/nervosnetwork/ckb/pulls");
+    let builder = builder.header(USER_AGENT, "foo");
+    let body = builder.send()?.text()?;
+
+
+
+   // let body = reqwest::blocking::get("https://api.github.com/repos/nervosnetwork/ckb/pulls")?.text()?;
+    println!("body = {:?}", body);
+    
+    //println!("test");
+    return Ok(());
+
+
+
+}
+
+
+
