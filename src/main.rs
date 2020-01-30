8d69529d188e14d0b6c5fac19bd8108b65c4d72e
coder-pig <aimeedeer@gmail.com>
2020-01-30 18:10:43 +0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
 /*
- *   Copyright (c) 2020 
+ *   Copyright (c) 2020
  *   All rights reserved.
  */
 use reqwest::blocking::Client;
-use reqwest::Method;
 use reqwest::header::USER_AGENT;
+use reqwest::Method;
 use serde_json::value::Value;
 
+static RIB_AGENT: &'static str = "";
 
-static RIB_AGENT:&'static str = "";
-
-fn main() -> Result<(), reqwest::Error>{
-
+fn main() -> Result<(), reqwest::Error> {
     let client = Client::new();
     let repos = include_str!("github-repos.txt");
-    
-    for repo in repos.lines()
-    {
-        println!("{}",repo);
-        let repourl = format!("https://api.github.com/repos/{}/pulls",repo); 
-        let builder = client.request(Method::GET,  &repourl );
-        let builder = builder.header(USER_AGENT, RIB_AGENT);
-        let body:Value = builder.send()?.json()?; 
-        println!("body = {:#?}", body); 
 
+    for repo in repos.lines() {
+        println!("{}", repo);
+        let repourl = format!("https://api.github.com/repos/{}/pulls", repo);
+        let builder = client.request(Method::GET, &repourl);
+        let builder = builder.header(USER_AGENT, RIB_AGENT);
+        let body: Value = builder.send()?.json()?;
+        println!("body = {:#?}", body);
     }
 
-    
     return Ok(());
 }
-
-
-
