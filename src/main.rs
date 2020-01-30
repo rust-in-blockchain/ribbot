75040c782212f5bbe322e475c1c749ac5cc9763e
coder-pig <aimeedeer@gmail.com>
2020-01-30 18:05:57 +0700

    test repo

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,31 @@ use reqwest::Method;
 use reqwest::header::USER_AGENT;
 use serde_json::value::Value;
 
+
 static RIB_AGENT:&'static str = "";
 
 fn main() -> Result<(), reqwest::Error>{
 
     let client = Client::new();
-    let builder = client.request(Method::GET, "https://api.github.com/repos/nervosnetwork/ckb/pulls");
-    let builder = builder.header(USER_AGENT, RIB_AGENT);
-    let body:Value = builder.send()?.json()?; 
+    let repos = include_str!("github-repos.txt");
+    //println!("{}", repos);
 
-    println!("body = {:#?}", body);
+    
+    for repo in repos.lines()
+    {
+        println!("{}",repo);
+        let repourl = format!("https://api.github.com/repos/{}/pulls",repo);
+        // println!("{}",repourl);
+
+
+
+        let builder = client.request(Method::GET,  &repourl );//"https://api.github.com/repos/nervosnetwork/ckb/pulls");
+        let builder = builder.header(USER_AGENT, RIB_AGENT);
+        let body:Value = builder.send()?.json()?; 
+        println!("body = {:#?}", body); 
+
+    }
+
     
     return Ok(());
 }
